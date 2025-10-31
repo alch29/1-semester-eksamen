47d6550da2eee61301357678f61f6cbcc52d4448
BuCh3R <mortenbuch@live.dk>
2025-10-31 11:01:36 +0100

    database data fetched

--- models/user.js
+++ models/user.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      user.hasOne(role, {foreignKey: 'role_id'});
+      user.hasOne(models.role, {foreignKey: 'role_id'});
     }
   }
   user.init({

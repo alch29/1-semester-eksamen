93d6dafd259c84f062cab2db76c54d612f727001
Trace <d.nguyen@live.dk>
2025-10-31 11:04:15 +0100

    asdf

--- models/image.js
+++ models/image.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      image.belongsTo(user, {foreignKey: 'user_id'});
+      image.belongsTo(models.user, {foreignKey: 'user_id'});
     }
   }
   image.init({

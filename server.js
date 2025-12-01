c4060f253e374905bdc5aef68d14784adc751829
Trace <d.nguyen@live.dk>
2025-12-01 09:28:57 +0100

    a

--- server.js
+++ server.js
@@ -10,21 +10,4 @@ app.listen(PORT, HOST, () => {
 });
 
 
-app.get('/admin/add-staff', (req, res) => {
-  res.render('admin/staff/admin-add-staff', {
-      title: 'Add new staff',
-  });
-});
-
-app.get('/admin/add-staff/stations', (req, res) => {
-  res.render('admin/staff/admin-add-staff-stations', {
-      title: 'Add new staff to stations',
-  });
-});
-
-app.get('/admin/staff-info', (req, res) => {
-  res.render('admin/staff/admin-staff-info', {
-      title: 'Staff info',
-  });
-});
 

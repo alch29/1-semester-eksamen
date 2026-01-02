c4841a50bce918a1e993b8c77cdf6fa012f3d48a
alch29 <christensen.alek@gmail.com>
2026-01-02 13:17:52 +0100

    disk storage og migationsfil

--- multer.js
+++ multer.js
@@ -1,7 +1,17 @@
 const multer = require('multer');
 const path = require('path');
 
-const storage = multer.memoryStorage();
+// const storage = multer.memoryStorage();
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'public/uploads/');
+    },
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        cb(null, uniqueSuffix + path.extname(file.originalname));
+    }
+});
 
 const fileFilter = (req, files, cb) => {
     const allowedTypes = /jpeg|jpg|png|gif|webp/;

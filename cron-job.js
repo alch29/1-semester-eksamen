a8eee42316fb566ca29f20e6c440cad270913e3b
alch29 <christensen.alek@gmail.com>
2026-01-06 15:08:56 +0100

    .

--- cron-job.js
+++ cron-job.js
@@ -1,57 +1,64 @@
-// const { image } = require('./models');
-// const { Op } = require('sequelize');
-// const fs = require('fs');
-// const path = require('path');
+const { image } = require('./models');
+const { Op } = require('sequelize');
+const fs = require('fs');
+const path = require('path');
 
 
-// const thirtyDaysAgo = new Date(Date.now() - 30 * 24 * 60 * 60 * 1000);
-// const imageDir = path.join(__dirname, 'public/uploads'); // mappe med billeder
-// //Tjekker om billeder er mere end 30 dage gamle og sletter dem:
+const days = 30;
+// const maxAge = days * 24 * 60 * 60 * 1000;
+const maxAge = 5 * 60 * 1000;
+const imageDir = path.join(__dirname, 'public/uploads'); // mappe med billeder
+//Tjekker om billeder er mere end 30 dage gamle og sletter dem:
 
-// function deleteOldImagesFromDisk() {
-//   const now = Date.now();
+function deleteOldImagesFromDisk() {
+  const now = Date.now();
 
-//   fs.readdir(imageDir, (err, files) => {
-//     if (err) {
-//       console.error('Fejl ved læsning af mappe:', err);
-//       return;
-//     }
+  fs.readdir(imageDir, (err, files) => {
+    if (err) {
+      console.error('Fejl ved læsning af mappe:', err);
+      return;
+    }
 
-//     files.forEach(file => {
-//       const filePath = path.join(imageDir, file);
+    files.forEach(file => {
+      const filePath = path.join(imageDir, file);
 
-//       fs.stat(filePath, (err, stats) => {
-//         if (err) return;
+      fs.stat(filePath, (err, stats) => {
+        if (err) return;
 
-//         const age = now - stats.mtimeMs;
+        const age = now - stats.mtimeMs;
 
-//         if (age > thirtyDaysAgo) {
-//           fs.unlink(filePath, err => {
-//             if (!err) {
-//               console.log('Slettet:', file);
-//             }
-//           });
-//         }
-//       });
-//     });
-//   });
-// }
+        if (age > maxAge) {
+          fs.unlink(filePath, err => {
+            if (!err) {
+              console.log('Deleted:', file);
+            }
+          });
+        }
+      });
+    });
+  });
+}
 
-// async function deleteOldImagesFromDatabase() {
-//   try {
+async function deleteOldImagesFromDatabase() {
+  try {
 
-//     const deletedCount = await image.destroy({
-//       where: {
-//         createdAt: {
-//           [Op.lt]: thirtyDaysAgo
-//         }
-//       }
-//     });
+    const deletedCount = await image.destroy({
+      where: {
+        createdAt: {
+          [Op.lt]: maxAge
+        }
+      }
+    });
 
-//     console.log(`Deleted ${deletedCount} old images`);
-//   } catch (err) {
-//     console.error('Error deleting old images:', err);
-//   }
-// }
+    console.log(`Deleted ${deletedCount} old images`);
+  } catch (err) {
+    console.error('Error deleting old images:', err);
+  }
+}
 
-// module.exports = deleteOldImages;
+async function cleanupOldImages() {
+    deleteOldImagesFromDisk();
+    await deleteOldImagesFromDatabase();
+  }
+
+module.exports = { cleanupOldImages };
